023e9efa531d5f7b178996d1ef34eb2578d8ee73
Baullin1767 <86959616+Baullin1767@users.noreply.github.com>
2024-04-10 13:52:33 +0300

    f

--- Battle/Battle/obj/Debug/net8.0/Battle.AssemblyInfo.cs
+++ Battle/Battle/obj/Debug/net8.0/Battle.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Battle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+858bd04ebe5b5c4e942d8538a80312b1fd313b38")]
 [assembly: System.Reflection.AssemblyProductAttribute("Battle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Battle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
